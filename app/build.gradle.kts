8c7aac9fbc5f4adf640c12fa7c867bd34d74964b
NORMss <serega.b301@gmail.com>
2025-01-09 20:33:57 +0700

    implemented token hiding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,12 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.serialization)
 }
 
 android {
     namespace = "ru.normno.myencrypteddatastore"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ru.normno.myencrypteddatastore"
@@ -40,6 +41,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.datastore.preferences)
+    implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/ru/normno/myencrypteddatastore/MainActivity.kt
+++ app/src/main/java/ru/normno/myencrypteddatastore/MainActivity.kt
@@ -1,47 +1,93 @@
 package ru.normno.myencrypteddatastore
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.datastore.dataStore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import ru.normno.myencrypteddatastore.ui.theme.MyEncryptedDataStoreTheme
 
+private val Context.dataStore by dataStore(
+    fileName = "user-preferences",
+    serializer = UserPreferencesSerializer,
+)
+
+//private val SECRET_TOKEN = (1..1000).map {
+//    (('a'..'z') + ('A'..'Z') + ('0'..'9')).random()
+//}.joinToString(separator = "")
+
+private const val SECRET_TOKEN = "Hello, Crypto!"
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val scope = rememberCoroutineScope()
+            var text by remember {
+                mutableStateOf("")
+            }
+
             MyEncryptedDataStoreTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Button(
+                            onClick = {
+                                scope.launch(Dispatchers.IO) {
+                                    dataStore.updateData {
+                                        UserPreferences(
+                                            token = SECRET_TOKEN
+                                        )
+                                    }
+                                }
+                            }
+                        ) {
+                            Text(
+                                text = "Encrypt"
+                            )
+                        }
+                        Button(
+                            onClick = {
+                                scope.launch(Dispatchers.IO) {
+                                    text = dataStore.data.first().token ?: "null"
+                                }
+                            }
+                        ) {
+                            Text(
+                                text = "Decrypt"
+                            )
+                        }
+                        Text(
+                            text = text
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyEncryptedDataStoreTheme {
-        Greeting("Android")
     }
 }
